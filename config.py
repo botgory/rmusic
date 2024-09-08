18ce0e3f05e78b55eadde143100f72b8dbacbf61
botsonn <ahmdghyly09@gmail.com>
2024-09-09 02:20:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-CHANNEL_NAME = getenv("CHANNEL_NAME", "تحديثات بوت رفا")
-CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/sourcerafa")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sourcerafa")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "تحديثات بوت ريان")
+CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/RyanBCH")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RyanBCH")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
